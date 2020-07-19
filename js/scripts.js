f30968cc0f28b25fd1c29e83e9b74cc17d7a07b4
Dinh Thai <dinh_k_thai@hotmail.com>
2020-07-18 23:57:34 -0600

    Clean up code, delete div and a elements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,3 @@
-{
-    console.log("Script starting!");
-
-    Get value : textObject.value
-    Set value : textObject.value = text
-
-
-    
     function myFunction() { 
         var x =  
             document.getElementById("myText").value; 
@@ -13,5 +5,5 @@
         document.getElementById( 
           "demo").innerHTML = x; 
     }
-}
+
 
