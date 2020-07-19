c299545f43fbc4c3c2042439b8271f986ebbca58
Dinh Thai <dinh_k_thai@hotmail.com>
2020-07-18 19:37:42 -0600

    Update contact menu.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
+{
+    console.log("Script starting!");
+
+    Get value : textObject.value
+    Set value : textObject.value = text
+
+
+    
+    function myFunction() { 
+        var x =  
+            document.getElementById("myText").value; 
+        
+        document.getElementById( 
+          "demo").innerHTML = x; 
+    }
+}
 
