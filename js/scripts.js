38222fb2c903206272fbd2aeaf42c14f52b73e50
Dinh Thai <dinh_k_thai@hotmail.com>
2020-07-20 02:33:50 -0600

    Delete capital letters, update readme.

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,9 @@ input.addEventListener("click", () =>
     //Citation: Email with javascript. stackoverflow.com
     //Link: https://stackoverflow.com/questions/14268796/how-do-i-send-email-with-javascript-without-opening-the-mail-client
     link = "mailto:dinh_k_thai@hotmail.com?subject=Message: "
-        +document.getElementById("Subject").value
-        +"&body="+document.getElementById("Message").value
-        +document.getElementById("Name").value
-        +document.getElementById("Email").value;
+        +document.getElementById("subject").value
+        +"&body="+document.getElementById("message").value
+        +document.getElementById("name").value
+        +document.getElementById("email").value;
         window.location.href = link;
 });
